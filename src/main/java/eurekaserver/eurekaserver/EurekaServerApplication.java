2597cc2bf090e5365708599a108e1c5147b8e478
920654003@qq.com <920654003@qq.com>
2020-08-21 12:12:50 +0800

    init commit

---
+++ src/main/java/eurekaserver/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package eurekaserver.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//启动类上添加此注解标识该服务为配置中心
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
